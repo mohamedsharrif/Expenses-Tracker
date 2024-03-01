e596a14f738a8844710d66c48dd3d6f5c070cbba
mohamedsharrif <sharifmohamedmahamud@gmail.com>
2024-03-01 03:35:14 +0200

    Updated TransactionList component to use useContext hook. Dynamically render transaction items using map function

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <div>
-       <h3>History</h3>
+      <h3>History</h3>
       <ul className="list">
-         <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
-      </ul>
+      {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} /> ))}
+     </ul>
     </div>
-  )
-}
+  );
+};
 
-export default TransactionList
+export default TransactionList;
